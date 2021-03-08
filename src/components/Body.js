d37e0c865da9952b352be30adee440acc117ca09
Laura Gwen <lauraburch.lb@gmail.com>
2021-03-07 19:19:28 -0500

    added email,photo, and cell to list

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,26 @@
-function Body({users}) {
+function Body({ users }) {
     return (
-        <div className="card" style={{ background: "pink", fontFamily:"cursive", margin: "2rem", borderRadius: "6px"}}>
-            <div className="card-body" style={{ display: "flex", flexWrap: "wrap" }}>
-                <h2 className="card-title" style={{textAlign:"center", fontWeight:"bolder", }}>Employees</h2>
-            </div>
-            <div>
-            {users.map(user => <h3 key={user.login.uuid} className="card-text">{user.name.first} {user.name.last}</h3>)}
+        <div className='container'>
+            <div className="card" style={{ background: "pink", fontFamily: "cursive", margin: "2rem", borderRadius: "6px" }}>
+                <div className="card-body" style={{ display: "flex", flexWrap: "wrap" }}>
+                    <h2 className="card-title" style={{ textAlign: "center", fontWeight: "bolder", }}>Employees</h2>
+                </div>
+                <ul className="container mr-md-3">
+                    {
+                        users.map
+                            (user =>
+                                <li key={user.login.uuid} className="card-text ml-md-5 pb-5">
+                                    <span className='font-weight-bold'>Employee Name: </span>{user.name.title} {user.name.first} {user.name.last}
+                                    <br />
+                                    <span className='font-weight-bold'>Employee Email: </span> {user.email}
+                                    <br />
+                                    <span className='font-weight-bold'>Employee Cell </span> {user.cell}
+                                    <br />
+                                    <img className='rounded' src={user.picture.large} alt="employee pic" />
+                                </li>
+                            )
+                    }
+                </ul>
             </div>
         </div>
     )
